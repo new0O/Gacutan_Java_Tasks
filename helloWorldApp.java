cca48e235f3d3e60c8174b2bcb2347da55b9b81f
Victor Owen Villena Gacutan <142770764+new0O@users.noreply.github.com>
2025-07-03 16:19:23 +0000

    tasksheet 1-1-1_Changed form Hello Word! to Kamusta Mundo!

---
+++ helloWorldApp.java
@@ -0,0 +1,10 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+public class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); //Display the string.
+    }
+}
