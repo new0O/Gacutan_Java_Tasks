26611054f2e87ad45dc0a5e7d4b2916b5541d691
Victor Owen Villena Gacutan <142770764+new0O@users.noreply.github.com>
2025-08-01 01:30:32 +0000

    Tasksheet-1-1-2

---
+++ tasks/Tasksheet112.java
@@ -0,0 +1,23 @@
+package tasks;
+
+public class Tasksheet112 {
+    public static void main(String[] args) {
+        int result = 1 + 2; // result is now 3
+
+        System.out.println(result);
+
+        result -= 1;                // result = 2
+        System.out.println(result);
+
+        result *= 2;                // result = 4
+        System.out.println(result);
+
+        result /= 2;                // result = 2
+        System.out.println(result);
+
+        result += 8;                // result = 10
+        result %= 7;                // result = 3
+        System.out.println(result);
+    }
+}
+
