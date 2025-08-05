4824a444557cf5069bb23143e4d0261eea1a3cde
Victor Owen Villena Gacutan <142770764+new0O@users.noreply.github.com>
2025-08-05 23:48:09 +0000

    Task2

---
+++ tasks/Task2.java
@@ -0,0 +1,18 @@
+package tasks;
+
+public class Task2 {
+     public static void main(String[] args) {
+        char charH = 'H';
+        byte byte3 = 3;
+        char charL = 'l'; // Used twice for 'll'
+        short short0_first = 0; // Using short for the '0' in H3ll0
+        int int0_second = 0;
+        float float2_0 = 2.0f;
+        boolean boolTrue = true;
+        String output = "" + charH + byte3 + charL + charL + short0_first + 
+                        " w" + int0_second + "rld " + float2_0 +  " " + boolTrue; 
+
+        System.out.println(output);
+    }
+
+}
